498b92d05ef40b79f1d29c7545c97c4df2ff51ec
shrynx <shriyansbhatnagar@gmail.com>
2025-04-08 01:33:16 +0200

    added loom

--- dotfiles/.finicky.js
+++ dotfiles/.finicky.js
@@ -1,7 +1,7 @@
 // Use https://finicky-kickstart.now.sh to generate basic configuration
 // Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
 
-const workApps = ["Mattermost.Desktop", "notion.id"];
+const workApps = ["Mattermost.Desktop", "notion.id", "com.loom.desktop"];
 
 module.exports = {
   options: {
@@ -15,7 +15,7 @@ module.exports = {
   },
   handlers: [
     {
-      // Open any link clicked in Mattermost in Google Chrome work profile
+      // Open any link clicked in any work app in Google Chrome work profile
       match: ({ opener }) => workApps.includes(opener.bundleId),
       browser: {
         name: "Google Chrome",
