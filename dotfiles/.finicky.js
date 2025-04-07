0d1cced8a2e697727e7bd84ab8cb76ef1b328425
shrynx <shriyansbhatnagar@gmail.com>
2025-04-07 20:26:13 +0200

    refactor finicky

--- dotfiles/.finicky.js
+++ dotfiles/.finicky.js
@@ -1,45 +1,36 @@
 // Use https://finicky-kickstart.now.sh to generate basic configuration
 // Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
 
-module.exports = {
-    options: {
-        hideIcon: true,
-        checkForUpdate: false,
-        logRequests: false,
-    },
-    defaultBrowser: {
-        name: "Google Chrome",
-        profile: "Default"
-    },
-    handlers: [
-        {
-            // Open any link clicked in Mattermost in Google Chrome work profile
-            match: ({ opener }) =>
-                opener.bundleId === "Mattermost.Desktop",
-            browser: {
-                name: "Google Chrome",
-                profile: "Profile 1"
-            }
-        },
-        {
-            // Open any link clicked in Notion in Google Chrome work profile
-            match: ({ opener }) =>
-                opener.bundleId === "notion.id",
-            browser: {
-                name: "Google Chrome",
-                profile: "Profile 1"
-            }
-        },
-        {
-            // Open meridia.* urls in Google Chrome work profile
-            match: [
-                "meridia*", // match meridia urls
-            ],
-            browser: {
-                name: "Google Chrome",
-                profile: "Profile 1"
-            }
-        }
-    ]
+const workApps = ["Mattermost.Desktop", "notion.id"];
 
-}
+module.exports = {
+  options: {
+    hideIcon: true,
+    checkForUpdate: false,
+    logRequests: false,
+  },
+  defaultBrowser: {
+    name: "Google Chrome",
+    profile: "Default",
+  },
+  handlers: [
+    {
+      // Open any link clicked in Mattermost in Google Chrome work profile
+      match: ({ opener }) => workApps.includes(opener.bundleId),
+      browser: {
+        name: "Google Chrome",
+        profile: "Profile 1",
+      },
+    },
+    {
+      // Open meridia.* urls in Google Chrome work profile
+      match: [
+        "*meridia*", // match meridia urls
+      ],
+      browser: {
+        name: "Google Chrome",
+        profile: "Profile 1",
+      },
+    },
+  ],
+};
