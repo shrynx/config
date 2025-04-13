4d7299183a07ee10801bb53cdc903745e5044d4c
shrynx <shriyansbhatnagar@gmail.com>
2025-04-13 21:22:04 +0200

    added config to procs

--- home.nix
+++ home.nix
@@ -21,6 +21,8 @@
     ".finicky.js".source = ./dotfiles/.finicky.js;
     ".gitconfig".source = ./dotfiles/.gitconfig;
     ".gitignore".source = ./dotfiles/.gitignore;
+    ".procs.toml".source = ./dotfiles/.procs.toml;
+    ".hushlogin".source = ./dotfiles/.hushlogin;
     ".warp" = {
       source = ./dotfiles/warp;
       recursive = true;
